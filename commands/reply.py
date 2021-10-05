3357195028db06cdc1299c42b0f9ef05d1adb0fe
Felix Gordon <78864962+fxGRDN@users.noreply.github.com>
2021-10-05 18:10:11 +0200

    DRYed code and added mentioning to hi command

--- commands/reply.py
+++ commands/reply.py
@@ -1,22 +1,23 @@
 from discord.ext import commands
 from lib.fetchimg import getRandImg
-from lib.randhi import randHi, randBingus
+from lib.randhi import randText
+
 
 @commands.command(brief="Przywitanie")
 async def hi(ctx):
-    await ctx.send(randHi())
+    await ctx.send(randText("hi.txt") + ', ' + str(ctx.author.mention))
+
 
 @commands.command(brief="Mój stosunek do bingus")
 async def bingus(ctx):
-    await ctx.send(randBingus())
+    await ctx.send(randText("bingus.txt"))
+
 
 @commands.command(brief="Najwyższa prawda")
-async  def php(ctx):
-    await  ctx.send("jebac php fest")
+async def php(ctx):
+    await ctx.send("jebac php fest")
+
 
 @commands.command(brief="Wspaniale fotografie")
-async  def flop(ctx):
+async def flop(ctx):
     await ctx.send(getRandImg())
-
-
-
--- lib/randhi.py
+++ lib/randhi.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 d_f = Path(__file__).resolve().parent
 
-def randHi():
 
-    hi_file = d_f / "hi.txt"
+def randText(file):
+    hi_file = d_f / file
 
     f = open(hi_file, encoding='utf-8')
 
@@ -13,18 +13,4 @@ def randHi():
 
     rand = line.split(';')
 
-    return rand[randint(0, len(rand)-1)]
-
-
-def randBingus():
-
-    bing_file = d_f / "bingus.txt"
-
-    f = open(bing_file, encoding='utf-8')
-
-    line = f.read()
-
-    rand = line.split(', ')
-
     return rand[randint(0, len(rand) - 1)]
-
