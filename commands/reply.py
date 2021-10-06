80ae74cbc7e3410d3a763c289bbfab2f3a9d4dfa
Felix Gordon <78864962+fxGRDN@users.noreply.github.com>
2021-10-06 18:13:00 +0200

    Revert "DRYed code and added mentioning to hi command"
    This reverts commit 3357195028db06cdc1299c42b0f9ef05d1adb0fe.

--- commands/reply.py
+++ commands/reply.py
@@ -1,23 +1,22 @@
 from discord.ext import commands
 from lib.fetchimg import getRandImg
-from lib.randhi import randText
-
+from lib.randhi import randHi, randBingus
 
 @commands.command(brief="Przywitanie")
 async def hi(ctx):
-    await ctx.send(randText("hi.txt") + ', ' + str(ctx.author.mention))
-
+    await ctx.send(randHi())
 
 @commands.command(brief="Mój stosunek do bingus")
 async def bingus(ctx):
-    await ctx.send(randText("bingus.txt"))
-
+    await ctx.send(randBingus())
 
 @commands.command(brief="Najwyższa prawda")
-async def php(ctx):
-    await ctx.send("jebac php fest")
-
+async  def php(ctx):
+    await  ctx.send("jebac php fest")
 
 @commands.command(brief="Wspaniale fotografie")
-async def flop(ctx):
+async  def flop(ctx):
     await ctx.send(getRandImg())
+
+
+
--- lib/randhi.py
+++ lib/randhi.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 d_f = Path(__file__).resolve().parent
 
+def randHi():
 
-def randText(file):
-    hi_file = d_f / file
+    hi_file = d_f / "hi.txt"
 
     f = open(hi_file, encoding='utf-8')
 
@@ -13,4 +13,18 @@ def randText(file):
 
     rand = line.split(';')
 
+    return rand[randint(0, len(rand)-1)]
+
+
+def randBingus():
+
+    bing_file = d_f / "bingus.txt"
+
+    f = open(bing_file, encoding='utf-8')
+
+    line = f.read()
+
+    rand = line.split(', ')
+
     return rand[randint(0, len(rand) - 1)]
+
